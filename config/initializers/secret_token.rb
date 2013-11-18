7b9a4f39c31bf1cb36d1a87d9eafb33f4f8dc44e
Shehzad Shahbuddin <shehzad.shahbuddin@gmail.com>
2013-11-17 19:50:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projectwip::Application.config.secret_key_base = '683f5da95df7509c257450a0b27fd4409de11e69ff97a1b68b31e21fc4a13757fd519772c3e5f691351095d356761283bfb548fcd88125a3ab796bd621df63f1'
