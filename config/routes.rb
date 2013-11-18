0c1f70f94d4edce7545c64eddca21ef2473064b9
Shehzad Shahbuddin <shehzad.shahbuddin@gmail.com>
2013-11-18 17:09:03 -0500

    Bootstrap Layout in place

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Projectwip::Application.routes.draw do
+root "pages#home"
+get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
